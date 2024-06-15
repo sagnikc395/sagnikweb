87ce0f88d5855e61f85fb5a7fd89bcf4fea4a8c5
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-06-16 02:22:17 +0530

    src: added custom 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="error">
+	<h1>👽 {$page.status} : {$page.error?.message}</h1>
+</div>
+
+<style>
+	.error {
+		height: 100%;
+		display: grid;
+		place-content: center;
+	}
+</style>
